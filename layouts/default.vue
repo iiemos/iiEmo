cd0ede42fe690d5c625886420ae52241c25519b6
Aiske <2254749998@qq.com>
2022-09-01 01:16:00 +0800

    0901-添加时钟

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,5 @@
-<template>
-  <div class="_layouts">
-    <Header />
-      <section :class="$style['_layouts_slot']">
-        <slot />
-        <BackTop />
-      </section>
-    <!-- <Spotlight /> -->
-    <Footer />
-  </div>
-</template>
 <script setup lang="ts">
+import { ElNotification } from 'element-plus/dist/index.full.js'
 import { useStorage } from '@vueuse/core'
 import { storeToRefs } from 'pinia'
 let  { isConsole } = storeToRefs(iiemoStore())
@@ -19,18 +9,32 @@ if(!IP.value){
   IP.value = userData  
 }
 const setConsloe = () =>{
-  console.log('\n%c一朵大呲花 - 林深时见鹿 🍀\n', 'font-weight: 600;')
-  if(IP.value) console.log("🌰 Hi，来自 %c" + JSON.parse(IP.value).cname + " %c的朋友！", "color: #50a1ff; font-family: monoscope; font-weight: bold", "")
-  console.log('\n%c🍄 网站介绍：\n', 'font-weight: 600;')
-  console.log('\n🥕 纯静态： Nuxt3 + Vite + @nuxt/content + windi + 几个Element-Plus 组件 + 手撸\n')
-  console.log('\n🥕 UI： 乱七八糟随便整的\n')
-  console.log('\n🥕 初衷： 改善之前的wordPress笨重博客\n')
-  console.log('\n🥕 建立时间： 2022-08-01（建军节）有幸服过两年兵役\n')
-  console.log('\n%c想拥有自己博客吗？\n', 'font-weight: 600;')
-  console.log('\n本博客github完全开源，完全以配置文件的形式进行搭建，%c'+ '开箱即用' +'%c！\n', "color: #50a1ff; font-family: monoscope; font-weight: bold", "")
-  console.log('\n%c'+ '欢迎添加友链哦~~~~~~~~~~' +'%c\n', "color: #50a1ff; font-family: monoscope; font-weight: bold", "")
+  // console.log('\n%c一朵大呲花 - 林深时见鹿 🍀\n', 'font-weight: 600;')
+  // if(IP.value) console.log("🌰 Hi，来自 %c" + JSON.parse(IP.value).cname + " %c的朋友！", "color: #50a1ff; font-family: monoscope; font-weight: bold", "")
+  // console.log('\n%c🍄 网站介绍：\n', 'font-weight: 600;')
+  // console.log('\n🥕 纯静态： Nuxt3 + Vite + @nuxt/content + windi + 几个Element-Plus 组件 + 手撸\n')
+  // console.log('\n🥕 UI： 乱七八糟随便整的\n')
+  // console.log('\n🥕 初衷： 改善之前的wordPress笨重博客\n')
+  // console.log('\n🥕 建立时间： 2022-08-01（建军节）有幸服过两年兵役\n')
+  // console.log('\n%c想拥有自己博客吗？\n', 'font-weight: 600;')
+  // console.log('\n本博客github完全开源，完全以配置文件的形式进行搭建，%c'+ '开箱即用' +'%c！\n', "color: #50a1ff; font-family: monoscope; font-weight: bold", "")
+  // console.log('\n%c'+ '欢迎添加友链哦~~~~~~~~~~' +'%c\n', "color: #50a1ff; font-family: monoscope; font-weight: bold", "")
+
+  // 消息弹框
+  let msgTlt = IP.value ? `来自<span style="color:#7c3aed;">${JSON.parse(IP.value).cname}</span>的` : ''
   let timer = setTimeout(()=>{
     isConsole.value = true
+    ElNotification({
+      duration: 0,
+      title: ``,
+      appendTo: 'footer',
+      dangerouslyUseHTMLString: true,
+      message:
+        `<div>
+          🥕 Hi, 你好呀,${msgTlt}朋友
+        </div>`,
+      position: 'bottom-right',
+    })
     clearTimeout(timer)
   },1000)
 }
@@ -39,6 +43,20 @@ if(!isConsole.value){
 }
 
 </script>
+<template>
+  <div class="_layouts">
+    <Header />
+    <ProgressClock />
+
+      <section :class="$style['_layouts_slot']">
+        <slot />
+        <BackTop />
+      </section>
+    <!-- <Spotlight /> -->
+    <Footer />
+  </div>
+</template>
+
 <style lang="scss" module>
 ._layouts_slot{
   margin-top: 1.25rem;
